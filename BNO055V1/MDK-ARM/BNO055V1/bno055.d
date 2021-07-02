c87e6fedc1899584804ded7ce5a5022c47d485ca
SalihSalurr <58612345+SalihSalurr@users.noreply.github.com>
2021-07-03 01:32:29 +0300

    v1

---
+++ BNO055V1/MDK-ARM/BNO055V1/bno055.d
@@ -0,0 +1,6 @@
+bno055v1\bno055.o: ..\Drivers\STM32F1xx_HAL_Driver\Src\bno055.c
+bno055v1\bno055.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/bno055.h
+bno055v1\bno055.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+bno055v1\bno055.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+bno055v1\bno055.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+bno055v1\bno055.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
---
+++ BNO055V1/MDK-ARM/BNO055V1/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+bno055v1\startup_stm32f103xb.o: startup_stm32f103xb.s
